0cca74d0b332025b078fa2451315c0073d99579e
doodoo <buki@mhand.net>
2023-06-01 19:58:45 +0900

    :chore Modify favor-v2

--- contracts/Favor.sol
+++ contracts/Favor.sol
@@ -1,17 +1,19 @@
-// SPDX-License-Identifier: NONE
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
 
-import './klaytn/token/KIP7/KIP7Pausable.sol';
-import './klaytn/token/KIP7/KIP7Metadata.sol';
-import './klaytn/ownership/Ownable.sol';
+import './KIP/token/KIP7/KIP7.sol';
+import './KIP/token/KIP7/extensions/KIP7Pausable.sol';
+import './access/Ownable.sol';
 
-contract Favor is KIP7Pausable, KIP7Metadata, Ownable {
+contract Favor is KIP7Pausable, Ownable {
   uint256 public constant FAVOR_SUPPLY_LIMIT = 3e8 ether; // 300,000,000
 
   constructor(
     string memory name,
     string memory symbol
-  ) public KIP7Metadata(name, symbol, 18) Ownable() {
+  ) KIP7(name, symbol) Ownable() {
+    _setupRole(PAUSER_ROLE, _msgSender());
+    _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     _mint(msg.sender, FAVOR_SUPPLY_LIMIT);
   }
 
