18eb8c213bd37692fdef247cf52f4a4cd5ca81a0
doodoo <buki@mhand.net>
2023-06-01 18:10:26 +0900

    :chore Remove Burnable Contract

--- contracts/Favor.sol
+++ contracts/Favor.sol
@@ -1,19 +1,17 @@
 // SPDX-License-Identifier: NONE
 pragma solidity ^0.5.0;
 
-import './klaytn/token/KIP7/KIP7Burnable.sol';
 import './klaytn/token/KIP7/KIP7Pausable.sol';
 import './klaytn/token/KIP7/KIP7Metadata.sol';
 import './klaytn/ownership/Ownable.sol';
 
-contract Favor is KIP7Burnable, KIP7Pausable, KIP7Metadata, Ownable {
+contract Favor is KIP7Pausable, KIP7Metadata, Ownable {
   uint256 public constant FAVOR_SUPPLY_LIMIT = 3e8 ether; // 300,000,000
 
   constructor(
     string memory name,
-    string memory symbol,
-    uint8 decimal
-  ) public KIP7Metadata(name, symbol, decimal) Ownable() {
+    string memory symbol
+  ) public KIP7Metadata(name, symbol, 18) Ownable() {
     _mint(msg.sender, FAVOR_SUPPLY_LIMIT);
   }
 
